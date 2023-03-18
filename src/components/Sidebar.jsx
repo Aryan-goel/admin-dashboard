0b28d77e67706abc612cad8672d515c81747a285
91639 <goel.aryan2406@gmail.com>
2023-03-18 17:17:51 +0530

    [add]navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,12 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu,screenSize } = useStateContext();
+  const handleCloseSideBar=()=>{
+    if(activeMenu && screenSize<=900){
+      setActiveMenu(false);
+    }
+  }
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normal = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light m2'
   return (
@@ -15,7 +20,7 @@ const Sidebar = () => {
       {activeMenu && (<>
         <div className="flex justify-between items-center">
 
-          <Link to='/' onClick={() => { setActiveMenu(false) }} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+          <Link to='/' onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware />
             <span>
               Shoppy
@@ -35,7 +40,7 @@ const Sidebar = () => {
                 {item.title}
               </p>
               {item.links.map((link) => (
-                <NavLink to={`/${link.name}`} onClick={() => { }} className={({ isActive }) => isActive ? activeLink : normal}>
+                <NavLink to={`/${link.name}`} onClick={handleCloseSideBar} className={({ isActive }) => isActive ? activeLink : normal}>
                   {link.icon}
                   <span className="capitalize">
                     {link.name}
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,28 +1,38 @@
-import react,{createContext,useContext,useState} from 'react';
+import react, { createContext, useContext, useState } from 'react';
 
-const StateContext =createContext();
-const initialState={
-    chat:false,
-    cart:false,
-    userProfile:false,
-    notofication:false,
+const StateContext = createContext();
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
 
 
 }
 
-export const ContextProvider=({children})=>{
+export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(true);
 
-    return(
-        <StateContext.Provider 
-        value={{
-            activeMenu,
-            setActiveMenu
-        }} 
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true });
+    }
+    return (
+        <StateContext.Provider
+            value={{
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize
+            }}
         >
-            {children} 
+            {children}
         </StateContext.Provider>
     )
 
 }
-export const useStateContext=()=>useContext(StateContext);
+export const useStateContext = () => useContext(StateContext);
